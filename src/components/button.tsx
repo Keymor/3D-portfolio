1cf7c6b67dd4d6e3dfdb0d1ebc1451d5f686ab67
Keymor <vlad.kutuzov96@gmail.com>
2025-05-01 23:42:34 +0700

    page novigation

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,17 @@
 interface ButtonProps {
-    text: string
+    text: string,
+    scrollNum: number
 }
 
-export default function Button({text}: ButtonProps) {
+export default function Button({ text, scrollNum}: ButtonProps) {
+
+    const windowPlace = () => {
+        const screen = window.innerHeight * scrollNum
+        window.scrollTo({ top: screen, behavior: 'smooth' })
+    }
+
     return (
-        <button className="p-2 text-2xl text-white cursor-pointer relative">
+        <button onClick={windowPlace} className="p-2 text-2xl text-white cursor-pointer relative">
             <div className="min-w-full h-9 absolute group">
                 <div className="min-w-[110%]  h-[0.15rem] bg-white absolute -bottom-1 -translate-x-[12%] hidden group-hover:block" />
             </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,17 @@
 import Button from "./button"
 
 export default function Header() {
+
     return (
-        <div className="fixed w-full h-25 flex border-2 border-white border-x-0 bg-red-500 mt-5 z-10">
-            <div className="w-79 h-45 rounded-2xl [clip-path:polygon(0_0,100%_0%,70%_100%,30%_100%)] bg-red-500 ml-11 border-b-2 border-white" />
+        <div className="fixed w-full h-25 flex border-2 border-white border-x-0 bg-red-400 mt-5 z-10">
+            <div className="w-79 h-45 rounded-2xl [clip-path:polygon(0_0,100%_0%,70%_100%,30%_100%)] bg-red-400 ml-11 border-b-2 border-white" />
             <div className="flex flex-row mr-22 ml-auto my-auto h-fit gap-10 text-2xl">
-                <Button text="Home" />
-                <Button text="Portfolio" />
-                <Button text="About" />
-                <Button text="Contacts" />
+                <Button text="Home" scrollNum={0}/>
+                <Button text="Portfolio" scrollNum={1}/>
+                <Button text="About" scrollNum={2}/>
+                <Button text="Contacts" scrollNum={3}/>
             </div>
         </div>
     )
 }
+
