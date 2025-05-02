5b05792a804043d56554b40c6c6fed343811266f
Keymor <vlad.kutuzov96@gmail.com>
2025-05-02 21:44:02 +0700

    portfolio page update

--- src/components/imageBox.tsx
+++ src/components/imageBox.tsx
@@ -1,12 +1,19 @@
 interface ComponentProps {
-    ofset: number
+    ofset: number,
+    img: string,
+    positionX: number,
+    positionY: number,
+    scale: number,
+    bgColor: string
 }
 
-export default function ImageBox({ofset}: ComponentProps) {
+export default function ImageBox({ ofset, img, positionX, positionY, scale, bgColor }: ComponentProps) {
     return (
-        <div style={{transform: `translateY(${ofset}vh)`}} className="h-[68vh] w-[18vw] bg-gray-500 rounded-[4rem] text-center">
-            <img src="" alt="" />
-            Image holder
+        <div style={{ transform: `translateY(${ofset}vh)`, backgroundColor: bgColor }} className="h-[68vh] w-[18vw] bg-gray-500 rounded-[4rem] text-center shadow-2xl/70">
+            <img
+                style={{ backgroundImage: img, backgroundPosition: `${positionX}vw ${positionY}vh`, backgroundSize: `${scale}%` }}
+                alt=""
+                className="opacity-50 hover:opacity-100 cursor-pointer duration-300 bg-no-repeat size-full rounded-[4rem] bg-cover" />
         </div>
     )
 }
--- src/pagesComponent/portfolio.tsx
+++ src/pagesComponent/portfolio.tsx
@@ -5,10 +5,10 @@ export default function Portfolio() {
         <div className="w-screen h-screen bg-white flex flex-col inset-shadow-[0_20px_30px_rgb(0,0,0,0.3)] relative">
             <div className="h-25"/>
             <div className="flex flex-row gap-11 mt-30 mx-auto items-end z-2">
-                <ImageBox ofset={0} />
-                <ImageBox ofset={-7} />
-                <ImageBox ofset={-1} />
-                <ImageBox ofset={-6} />
+                <ImageBox bgColor="rgb(32, 32, 32)" scale={500} positionX={-38} positionY={0} img="url(/homeImg1.jpg)" ofset={0} />
+                <ImageBox bgColor="rgb(200, 6, 6)" scale={420} positionX={-37} positionY={0} img="url(/homeImg2.jpg)" ofset={-7} />
+                <ImageBox bgColor="rgb(32, 32, 32)" scale={420} positionX={-40} positionY={0} img="url(/homeImg3.jpg)" ofset={-1} />
+                <ImageBox bgColor="rgb(200, 6, 6)" scale={280} positionX={-17} positionY={-5} img="url(/sword.jpg)" ofset={-6} />
                 <div className="-translate-y-[7.5vh] h-[68vh] w-[8vw] rounded-[4rem] text-center relative">
                     <h1 className="rotate-90 left-13 absolute justify-center w-fit origin-bottom-left fontPortfolio leading-0 whitespace-nowrap">PORTFOLIO</h1>
                 </div>
