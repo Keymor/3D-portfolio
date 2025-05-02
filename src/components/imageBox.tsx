2f62a11369c824be0df1f68f3b7c1a5040505069
Keymor <vlad.kutuzov96@gmail.com>
2025-05-03 01:07:20 +0700

    size hover

--- src/components/imageBox.tsx
+++ src/components/imageBox.tsx
@@ -9,7 +9,7 @@ interface ComponentProps {
 
 export default function ImageBox({ ofset, img, positionX, positionY, scale, bgColor }: ComponentProps) {
     return (
-        <div style={{ transform: `translateY(${ofset}vh)`, backgroundColor: bgColor }} className="h-[68vh] w-[18vw] bg-gray-500 rounded-[4rem] text-center shadow-2xl/70">
+        <div style={{ transform: `translateY(${ofset}vh)`, backgroundColor: bgColor }} className="h-[68vh] w-[18vw] bg-gray-500 rounded-[4rem] text-center shadow-2xl/70 hover:scale-102 duration-300">
             <img
                 style={{ backgroundImage: img, backgroundPosition: `${positionX}vw ${positionY}vh`, backgroundSize: `${scale}%` }}
                 alt=""
