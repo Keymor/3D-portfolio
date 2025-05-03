ecbaff296ad282041aa52c39a7fbcb9a1ba0a288
Keymor <vlad.kutuzov96@gmail.com>
2025-05-03 13:09:42 +0700

    about page update

--- src/pagesComponent/about.tsx
+++ src/pagesComponent/about.tsx
@@ -1,13 +1,25 @@
 export default function About() {
     return (
-        <div className="w-screen h-screen bg-[rgb(40,36,33,1)] flex flex-row relative gap-[15vw]">
-            <div className="flex flex-col gap-[5vh] ml-auto my-auto">
-                <h1 className="text-9xl font-bold">ABOUT ME</h1>
-                <p className="w-[40vw] h-100 bg-gray-600 text-center">some text</p>
+        <div className="w-screen h-screen bg-[rgb(40,36,33,1)] flex flex-row relative gap-[20vw]">
+            <div className="flex flex-col gap-[5vh] ml-auto my-auto z-1">
+                <h1 className="text-9xl font-bold fontAbout relative text-white">ABOUT ME</h1>
+                <p style={{letterSpacing: '0.1rem', lineHeight: '1.6rem'}} className="w-[30vw] h-100 text-white fontFam">
+                    Hi! I'm Alexey Petrov — a passionate 3D artist and<br/>
+                    prop designer.<br/>
+                    I specialize in creating detailed models, bringing ideas to life<br/>
+                    with precision and creativity.<br/>
+                    From realistic grenades to vintage crates,I love adding story<br/>
+                    and soul into every piece.<br/>
+                    Always learning, always evolving —<br/>
+                    let’s create something amazing together!</p>
             </div>
             <div className="flex mr-auto mt-auto mb-[3vh]">
-                <div className="w-[30vw] h-[80vh] bg-gray-600 rounded-4xl text-center">photo</div>
+                <div style={{backgroundSize: '150%', backgroundPosition: '55% 36%'}} className="w-[30vw] h-[80vh] bg-gray-600 rounded-4xl bg-[url('/photo.jpg')] shadow-2xl/100"/>
             </div>
+            <div className="absolute bg-no-repeat top-60 left-209 size-50 bg-contain bg-[url('/figP1.png')]"/>
+            <div className="absolute bg-no-repeat top-80 right-0 h-70 w-35 bg-contain bg-[url('/figP2.png')]"/>
+            <div className="absolute bg-no-repeat bottom-80 left-10 size-15 bg-contain bg-[url('/figP3.png')]"/>
+            <div className="absolute bg-no-repeat bottom-0 left-1/2 h-50 w-52 bg-contain bg-[url('/figP4.png')]"/>
         </div>
     )
 }
--- src/pagesComponent/portfolio.tsx
+++ src/pagesComponent/portfolio.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import ImageBox from "../components/imageBox";
 
 export default function Portfolio() {
+
+    const [slide, setSlide] = useState(0)
+
     return (
         <div className="w-screen h-screen bg-white flex flex-col inset-shadow-[0_20px_30px_rgb(0,0,0,0.3)] relative">
-            <div className="h-25"/>
+            <div className="h-25" />
             <div className="flex flex-row gap-11 mt-30 mx-auto items-end z-2">
                 <ImageBox bgColor="rgb(32, 32, 32)" scale={500} positionX={-38} positionY={0} img="url(/homeImg1.jpg)" ofset={0} />
                 <ImageBox bgColor="rgb(200, 6, 6)" scale={420} positionX={-37} positionY={0} img="url(/homeImg2.jpg)" ofset={-7} />
@@ -13,12 +17,21 @@ export default function Portfolio() {
                     <h1 className="rotate-90 left-13 absolute justify-center w-fit origin-bottom-left fontPortfolio leading-0 whitespace-nowrap">PORTFOLIO</h1>
                 </div>
             </div>
-            <div className="text-black m-auto">- o o o -</div>
-            <div className=" absolute bg-[url('/fig1.png')] bg-cover bottom-10 left-15 w-39 h-40 z-1"/>
-            <div className=" absolute bg-[url('/fig2.png')] bg-no-repeat bg-contain bottom-10 right-20 size-22 z-0"/>
-            <div className=" absolute bg-[url('/fig3.png')] bg-no-repeat bg-contain -bottom-20 size-60 z-0"/>
-            <div className=" absolute bg-[url('/fig4.png')] bg-no-repeat bg-contain top-34 right-3/10 size-37 z-0"/>
-            <div className=" absolute bg-[url('/fig5.png')] bg-no-repeat bg-contain top-10 right-0 h-45 w-30 z-0"/>
+            <div className=" absolute bg-[url('/fig1.png')] bg-cover bottom-10 left-15 w-39 h-40 z-1" />
+            <div className=" absolute bg-[url('/fig2.png')] bg-no-repeat bg-contain bottom-10 right-20 size-22 z-0" />
+            <div className=" absolute bg-[url('/fig3.png')] bg-no-repeat bg-contain -bottom-20 size-60 z-0" />
+            <div className=" absolute bg-[url('/fig4.png')] bg-no-repeat bg-contain top-34 right-3/10 size-37 z-0" />
+            <div className=" absolute bg-[url('/fig5.png')] bg-no-repeat bg-contain top-10 right-0 h-45 w-30 z-0" />
+            <div className=" m-auto flex flex-row gap-5 w-fit items-center opacity-90">
+                <div className="h-1 w-12 bg-black" />
+                <div onClick={() => setSlide(0)} className="cursor-pointer size-6 rounded-full bg-black flex">
+                    <div style={{ visibility: slide === 0 ? "visible" : "hidden" }} className="size-5 rounded-full bg-gray-400 m-auto" />
+                </div>
+                <div onClick={() => setSlide(100)} className="cursor-pointer size-6 rounded-full bg-black flex">
+                    <div style={{ visibility: slide === 100 ? "visible" : "hidden" }} className="size-5 rounded-full bg-gray-400 m-auto" />
+                </div>
+                <div className="h-1 w-12 bg-black" />
+            </div>
         </div>
     )
 }
