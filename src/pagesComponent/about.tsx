cf55239186c20b6d45e8611f6b571cad428a38f4
Keymor <vlad.kutuzov96@gmail.com>
2025-05-12 20:11:56 +0700

    mob UI update

--- src/pagesComponent/about.tsx
+++ src/pagesComponent/about.tsx
@@ -1,12 +1,12 @@
 export default function About() {
     return (
-        <div className="w-screen h-screen bg-[rgb(40,36,33,1)] flex flex-row relative pt-30">
+        <div className="w-screen h-screen bg-[rgb(40,36,33,1)] flex flex-col sm:flex-row relative pt-30">
             <div className="flex flex-col gap-[5vh] m-auto z-1">
-                <div className="relative">
-                    <h1 className="text-[12rem] font-bold fontAbout text-white z-9 relative">ABOUT ME</h1>
-                    <div className="absolute bg-no-repeat -top-20 left-140 size-50 bg-contain bg-[url('/figP1.png')] z-1" />
+                <div className=" relative flex">
+                    <h1 className="text-8xl sm:text-[12rem] font-bold fontAbout text-white z-9 relative">ABOUT ME</h1>
+                    <div className="absolute bg-no-repeat sm:-top-20 sm:left-140 sm:size-50 size-20 left-80 -top-10 bg-contain bg-[url('/figP1.png')] z-1" />
                 </div>
-                <p style={{ letterSpacing: '0.1rem', lineHeight: '1.6rem' }} className="w-[45vw] h-[32vh] text-white fontFam">
+                <p className=" tracking-tight leading-4.5 sm:leading-[1.6rem] flex w-[90vw] line sm:w-[45vw] h-fit text-white fontFam">
                     Hi! I'm Alexey Petrov — a passionate 3D artist and<br />
                     prop designer.<br />
                     I specialize in creating detailed models, bringing ideas to life<br />
@@ -16,12 +16,12 @@ export default function About() {
                     Always learning, always evolving —<br />
                     let’s create something amazing together!</p>
             </div>
-            <div className="flex mr-auto my-auto">
-                <div style={{ backgroundSize: '150%', backgroundPosition: '55% 36%' }} className="w-[30vw] h-[78vh] bg-gray-600 rounded-4xl bg-[url('/photo.jpg')] shadow-2xl/100 z-1" />
+            <div className="flex mb-auto mx-auto sm:mr-auto sm:my-auto">
+                <div style={{ backgroundSize: '150%', backgroundPosition: '55% 30%' }} className="w-[80vw] h-[30vh] sm:w-[30vw] sm:h-[78vh] bg-gray-600 rounded-4xl bg-[url('/photo.jpg')] shadow-2xl/100 z-1" />
             </div>
-            <div className="absolute bg-no-repeat top-60 right-0 h-50 w-30 bg-contain bg-[url('/figP2.png')] z-0" />
+            <div className="absolute bg-no-repeat top-120 sm:top-60 right-0 h-50 w-30 bg-contain bg-[url('/figP2.png')] z-0" />
             <div className="absolute bg-no-repeat bottom-30 left-10 size-15 bg-contain bg-[url('/figP3.png')]" />
-            <div className="absolute bg-no-repeat bottom-0 left-1/2 h-50 w-52 bg-contain bg-[url('/figP4.png')]" />
+            <div className="absolute bg-no-repeat bottom-0 left-1/7 sm:left-1/2 h-50 w-52 bg-contain bg-[url('/figP4.png')]" />
         </div>
     )
 }
--- src/pagesComponent/contact.tsx
+++ src/pagesComponent/contact.tsx
@@ -29,42 +29,42 @@ export default function Contact() {
 
     return (
         <div className="w-screen h-screen bg-[rgb(221,11,20)] flex flex-col relative">
-            <img className="absolute h-[91px] w-[243px] scale-92 top-28 left-20 z-9" src={headerWhite} alt=""></img>
-            <div className="w-full h-30 bg-white" />
-            <div className="w-[220px] h-[60vh] my-auto ml-23 absolute top-1/4 left-0">
-                <h1 className="fontContact leading-[8vw] ml-auto mr-auto pl-[1vw]">CONTACT</h1>
+            <div className=" absolute top-0 left-0 w-full h-25 sm:h-30 bg-white" />
+            <img className="absolute h-[91px] w-[243px] scale-55 sm:scale-92 top-18 sm:top-28 -left-8 sm:left-20 z-9" src={headerWhite} alt=""></img>
+            <div className="sm:w-[220px] w-[40vw] h-[60vh] my-auto  sm:ml-23 absolute top-1/6 sm:top-1/4 left-0">
+                <h1 className="fontContact text-[30vw] sm:text-[12vw] leading-[8vw] ml-auto mr-auto pl-[1vw]">CONTACT</h1>
             </div>
-            <div className="flex relative my-auto pt-25 z-1">
-                <div className="flex flex-row ml-[25%]">
+            <div className="flex flex-col sm:flex-row relative mt-22 sm:my-auto pt-25 z-1">
+                <div className="flex flex-row ml-[35vw] sm:ml-[25%] relative">
                     <form ref={form} onSubmit={sendEmail} className="w-[50vw] h-[40vh] my-auto flex flex-col">
-                        <div style={{ letterSpacing: '0.1rem' }} className="w-fit gap-x-[5vw] h-2/3 grid grid-cols-2">
+                        <div style={{ letterSpacing: '0.1rem' }} className="w-fit gap-x-[5vw] h-2/3 grid sm:grid-cols-2 grid-cols">
                             <div className="w-fit h-fit flex flex-col fontFamLogo gap-1">
                                 <h2>Name *</h2>
                                 <input required name="name" type="text" className="outline-0"></input>
-                                <div className="w-[15vw] h-0.5 bg-white" />
+                                <div className="w-[60vw] sm:w-[15vw] h-0.5 bg-white" />
                             </div>
                             <div className="w-fit h-fit flex flex-col fontFamLogo gap-1">
                                 <h2>Company</h2>
                                 <input name='company' type="text" className="outline-0"></input>
-                                <div className="w-[15vw] h-0.5 bg-white" />
+                                <div className="w-[60vw] sm:w-[15vw] h-0.5 bg-white" />
                             </div>
                             <div className="w-fit h-fit flex flex-col fontFamLogo gap-1">
                                 <h2>E-mail *</h2>
                                 <input required name="email" type="email" className="outline-0"></input>
-                                <div className="w-[15vw] h-0.5 bg-white" />
+                                <div className="w-[60vw] sm:w-[15vw] h-0.5 bg-white" />
                             </div>
                             <div className="w-fit h-fit flex flex-col fontFamLogo gap-1">
                                 <h2>Phone</h2>
                                 <input name="phone" type="text" className="outline-0"></input>
-                                <div className="w-[15vw] h-0.5 bg-white" />
+                                <div className="w-[60vw] sm:w-[15vw] h-0.5 bg-white" />
                             </div>
                         </div>
-                        <div className="w-[35vw] h-fit flex flex-col fontFamLogo gap-3 my-auto z-1">
+                        <div className="w-[60vw] sm:w-[35vw] h-fit flex flex-col fontFamLogo gap-3 mt-15 sm:my-auto z-1">
                             <h2>Whant to know more? Drop us a line.</h2>
                             <input name='lineField' type="text" className="outline-0"></input>
                             <div className="w-full h-0.5 bg-white z-1" />
                         </div>
-                        <button type="submit" className="p-3 min-w-38 translate-x-1/2 bg-[rgb(33,33,33)] rounded-4xl absolute -bottom-20 right-1/2 cursor-pointer flex flex-row z-10">
+                        <button type="submit" className="sm:p-3 p-2 min-w-38 translate-x-1/2 bg-[rgb(33,33,33)] rounded-4xl absolute -bottom-30 sm:-bottom-20 right-1/3 sm:right-1/2 cursor-pointer flex flex-row z-10">
                             <div className="size-9 rounded-full bg-amber-50 my-auto flex" >
                                 <div className="size-8 bg-red-800 rounded-full m-auto" />
                             </div>
@@ -72,9 +72,8 @@ export default function Contact() {
                         </button>
                     </form>
                 </div>
-                <div className="w-[5vw] h-[30vh] ml-auto my-auto mr-[4vw] flex flex-col gap-4">
-
-                    <a href="https://www.artstation.com/petrov_alexey" target="_blank" className="cursor-pointer mx-auto mt-auto">
+                <div className="w-[5vw] h-[30vh] mt-45 ml-5 sm:ml-auto sm:my-auto sm:mr-[4vw] flex flex-row sm:flex-col gap-4">
+                    <a href="https://www.artstation.com/petrov_alexey" target="_blank" className="cursor-pointer sm:mx-auto sm:mt-auto">
                         <div className="size-15 bg-[url('/link1.png')] bg-contain bg-no-repeat" />
                     </a>
                     <a href="https://www.instagram.com/petrov.photography?igsh=MTVuZGp2MDgzYmU2Zg==" target="_blank" className="cursor-pointer mx-auto">
@@ -89,7 +88,7 @@ export default function Contact() {
                 </div>
             </div>
             <div className=" bg-[url('/logoBig.png')] h-[45vh] w-[27vw] absolute bottom-0 right-1/9 bg-no-repeat bg-cover opacity-25 z-0" />
-            <div className="bg-[url('/figCont.png')] h-50 w-80 absolute top-[20vh] right-0 bg-no-repeat" />
+            <div className="bg-[url('/figCont.png')] scale-70 sm:scale-100 h-50 w-80 absolute top-11/13 right-1/2 sm:top-[20vh] sm:right-0 bg-no-repeat" />
             <div className="w-full h-10"/>
         </div>
     )
